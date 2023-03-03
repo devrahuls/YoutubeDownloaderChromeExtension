af7c55ea6333d556ecc5dec7d7a594f924156056
rahull56 <printfrahulsaw@gmail.com>
2023-03-03 17:05:10 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [link, setLink] = React.useState();
+  const [currentLink, setCurrentLink] = React.useState();
+
+  function handleChangeText(event) {
+    setLink(event.target.value);
+  }
+
+  function handleSubmit() {
+    setCurrentLink(link);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputField">
+        <input
+          type="text"
+          className="text--field"
+          placeholder="Paste your link here"
+          onChange={handleChangeText}
+        />
+
+        <button className="btn" onSubmit={handleSubmit}>
+          Submit
+        </button>
+      </div>
+      <iframe
+      title="This is unique title"
+        id="widgetApi"
+        src={`https://convert2mp3s.com/api/widget?url=${currentLink}`}
+        width="100%"
+        height="250px"
+        allowtransparency="true"
+        style={{ border: "none" }}
+        className="kuchbhi"
+      ></iframe>
     </div>
   );
 }
